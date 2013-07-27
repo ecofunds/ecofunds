de479069b8beeba41db722499195609658355784
Henrique Bastos <henrique@bastos.net>
2013-07-27 03:02:49 -0300

    Refator as tasks db.django.* de acordo com a implementação do db.mysql

--- fabfile/db/django.py
+++ fabfile/db/django.py
@@ -1,6 +1,5 @@
 # coding: utf-8
-import os
-from fabric.api import task, get, env, put, abort, run, cd, require, prefix
+from fabric.api import task, get, env, put, run, cd, require, prefix
 from unipath import Path
 from ..helpers import timestamp
 
@@ -8,43 +7,39 @@ from ..helpers import timestamp
 @task
 def dumpdata(apps_or_models=''):
     '''
-    Connect to Dreamhost server, generate a database dump and
-    download it.
+    Generate a database dump and download it.
 
-    Usage: fab stage db.dump
+    Usage: fab stage db.django.dump
     '''
     require('PROJECT', provided_by=['stage', 'production'])
 
-    remote_file = '%s/%s-%s.json.bz2' % (env.PROJECT.tmp, env.host, timestamp())
+    remote_file = '%(tmp)s/db-%(instance)s-%(project)s-' % env.PROJECT + timestamp() +'.sql.bz2'
 
     with cd(env.PROJECT.current):
         with prefix('source bin/activate'):
             run('python manage.py dumpdata %s --indent 4 | bzip2 -c  > %s' % (apps_or_models, remote_file))
-            get(remote_file, os.getcwd())
+            get(remote_file, '.')
 
 
 @task
 def loaddata(local_file):
     '''
-    Connect to Dreamhost server, upload a local database dump and
-    restore it.
+    Upload a local database dump and restore it.
 
-    Usage: fab stage db.restore:dumpfile.bz2
+    Usage: fab stage db.django.loaddata:dumpfile.bz2
     '''
-    require('PROJECT', provided_by=['stage', 'production'])
+    require('PROJECT')
 
-    local_file = Path(local_file)
-    remote_file = Path(env.PROJECT.tmp, local_file.name)
+    local_file = Path(local_file).absolute()
+    remote_file = Path(put(local_file, env.PROJECT.tmp, use_sudo=True)[0])
 
-    if put(local_file, remote_file).failed:
-        abort('Failed to upload "%s"' % local_file)
+    if remote_file.endswith('.bz2'):
+        sudo('bunzip2 ' + remote_file)
+        remote_file = remote_file.parent.child(remote_file.stem)
 
     with cd(env.PROJECT.current):
-        if '.bz2' in remote_file:
-            run('bunzip2 %s' % remote_file)
-            remote_file = remote_file.replace('.bz2', '')
-
         with prefix('source bin/activate'):
             run('python manage.py loaddata %s' % remote_file)
 
     run('rm %s' % remote_file)
+
