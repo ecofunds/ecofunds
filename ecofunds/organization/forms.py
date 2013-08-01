3cdf71cdedb5ef45791e457c6ec421df579c5a2f
Henrique Bastos <henrique@bastos.net>
2013-07-31 23:18:22 -0300

    Remove campos a mais do formulário de filtro.

--- ecofunds/organization/forms.py
+++ ecofunds/organization/forms.py
@@ -30,14 +30,6 @@ class OrganizationAdvancedSearchForm(AdvancedSearchForm):
     s_all_organizations = forms.BooleanField(label=_('All organizations'), widget=forms.CheckboxInput(attrs={'class':'check', 'value': 0}))
     s_country = forms.CharField(label=_('Country'), widget=forms.TextInput(attrs={'class':'combo','autocomplete':'off','placeholder': _('Enter the name of a country')}))
     s_state = forms.CharField(label=_('State'), widget=forms.TextInput(attrs={'class':'combo','autocomplete':'off','placeholder': _('Enter the name of a state')}))
-    s_investments_focus = forms.IntegerField(label=_('Investments focus'), widget=forms.Select(choices=ACTIVITY_CHOICES, attrs={'class':''}))
-    s_all_investments_focus = forms.BooleanField(label=_('All project activities focus'), widget=forms.CheckboxInput(attrs={'class':'check', 'value': 0}))
-    s_investment_date_from = forms.CharField(label=_('From'), widget=forms.TextInput(attrs={'class':'data'}))
-    s_investment_date_to = forms.CharField(label=_('To'), widget=forms.TextInput(attrs={'class':'data'}))
-    s_all_investment_date = forms.BooleanField(label=_('All investments'), widget=forms.CheckboxInput(attrs={'class':'check', 'value': 0}))
-    s_estimated_investments_value_from = forms.CharField(label=_('Show investments from:'), widget=forms.TextInput(attrs={'class':'numero'}))
-    s_estimated_investments_value_to = forms.CharField(label=_('To'), widget=forms.TextInput(attrs={'class':'numero'}))
-    s_all_investments_received = forms.BooleanField(label=_('All investments received'), widget=forms.CheckboxInput(attrs={'class':'check', 'value': 0}))
 
     def _force_data(self):
         if not hasattr(self, 'cleaned_data'):
