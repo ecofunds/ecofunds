f7fb01a0fb4747243d529660d4fb21178c72448a
Henrique Bastos <henrique@bastos.net>
2013-08-03 14:40:00 -0300

    Ajusta configurações de log

--- ecofunds/settings.py
+++ ecofunds/settings.py
@@ -252,9 +252,15 @@ AUTH_PROFILE_MODULE = 'user.UserProfile'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        }
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         },
         'console':{
@@ -272,6 +278,11 @@ LOGGING = {
         },
     },
     'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
         'maps': {
             'handlers': ['console'],
             'level': 'DEBUG',
