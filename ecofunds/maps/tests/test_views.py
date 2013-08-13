6ea157b19bb0082e306bf9e420aa19da7f8df87c
Felipe cruz <felipecruz@loogica.net>
2013-08-13 10:05:13 -0300

    Fix investment view test

--- ecofunds/maps/tests/test_views.py
+++ ecofunds/maps/tests/test_views.py
@@ -302,9 +302,10 @@ class InvestmentMapTest(MapFixture):
             u'lat': -22.5331067902,
             u'lng': -43.2435698976,
             u'total_investment': 1000000,
-            u'total_investment_str': "$ 1.000.000,00",
+            u'total_investment_str': u"$ 1.000.000,00",
             u'projects': [
                 {
+                    u'id': 1,
                     u'acronym': u"Projeto de Teste",
                     u'url': None,
                     u'entity_id': 1,
