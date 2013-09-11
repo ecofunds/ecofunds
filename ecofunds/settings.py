6d6f9d2972c4f6608ab155dc7e94178110310199
Henrique Bastos <henrique@bastos.net>
2013-09-11 04:35:06 -0300

    Muda a ordem de precedência dos template loaders.

--- ecofunds/settings.py
+++ ecofunds/settings.py
@@ -97,8 +97,8 @@ SECRET_KEY = config('SECRET_KEY')
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
+    'django.template.loaders.filesystem.Loader',
     #'django.template.loaders.eggs.Loader',
 )
 
