c4dc70a03441e339a9f84e0baa8fa8d6ef9fb9f9
Felipe cruz <felipecruz@loogica.net>
2013-10-20 16:00:49 -0200

    Add fabric crud backup task

--- fabfile/django.py
+++ fabfile/django.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from fabric.api import env, task, settings, hide, cd, run, prefix
+from fabric.api import env, task, settings, hide, cd, run, prefix, get
 
 
 @task
@@ -9,3 +9,12 @@ def manage(command):
         with cd(env.PROJECT.current):
             with prefix('source bin/activate'):
                 run('python manage.py %s' % command)
+
+@task
+def crud_backup():
+    with cd(env.PROJECT.current):
+        with prefix('source bin/activate'):
+            run('python manage.py dumpdata crud.Activity2 crud.Organization2 ' +
+                'crud.Project2 crud.Investment2 --indent=4 --format=json ' +
+                '> crud.json')
+            get('crud.json', 'crud.json')
