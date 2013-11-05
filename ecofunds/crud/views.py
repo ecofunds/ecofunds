35530873569fe9b1a206ed26e13a6a72859c49dc
Henrique Bastos <henrique@bastos.net>
2013-11-05 11:55:34 -0200

    Group detail templates

--- ecofunds/crud/views.py
+++ ecofunds/crud/views.py
@@ -6,15 +6,15 @@ from ecofunds.crud.models import Organization2, Project2, Investment2
 
 def project_detail(request, project_id):
     model = get_object_or_404(Project2, id=project_id)
-    return render(request, "project/new_detail.html", {'project': model})
+    return render(request, "crud/project_detail.html", {'project': model})
 
 def investment_detail(request, investment_id):
     model = get_object_or_404(Investment2, id=investment_id)
-    return render(request, "investment/new_detail.html", {'investment': model})
+    return render(request, "crud/investment_detail.html", {'investment': model})
 
 def organization_detail(request, organization_id):
     model = get_object_or_404(Organization2, id=organization_id)
-    return render(request, "organization/new_detail.html", {'organization': model})
+    return render(request, "crud/organization_detail.html", {'organization': model})
 
 # REPEATED CODE
 # THIS NEW VERSION IS ADAPTED FOR THE NEW MODELS
