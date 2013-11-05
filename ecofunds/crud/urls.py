049688010df5344945428174f3949118ad928da8
Henrique Bastos <henrique@bastos.net>
2013-11-05 21:12:24 -0200

    Add ending slash to detail patterns

--- ecofunds/crud/urls.py
+++ ecofunds/crud/urls.py
@@ -2,7 +2,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('ecofunds.crud.views',
-    url(r'^investments/(?P<pk>\d+)', 'investment_detail', name="investment_detail"),
-    url(r'^projects/(?P<pk>\d+)', 'project_detail', name="project_detail"),
-    url(r'^organizations/(?P<pk>\d+)', 'organization_detail', name="organization_detail"),
+    url(r'^investments/(?P<pk>\d+)/$', 'investment_detail', name="investment_detail"),
+    url(r'^projects/(?P<pk>\d+)/$', 'project_detail', name="project_detail"),
+    url(r'^organizations/(?P<pk>\d+)/$', 'organization_detail', name="organization_detail"),
 )
