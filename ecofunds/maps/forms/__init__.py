f0fa13e5fb5a75c431ad267c14d48725fa31fda8
Henrique Bastos <henrique@bastos.net>
2013-11-05 20:32:16 -0200

    Add filter by pk

--- ecofunds/maps/forms/__init__.py
+++ ecofunds/maps/forms/__init__.py
@@ -5,6 +5,7 @@ from ecofunds.crud.models import Organization2, Activity2, Investment2
 
 
 class OrganizationFilterForm(forms.Form):
+    pk = forms.IntegerField(required=False)
     name = forms.CharField(required=False)
     kind = forms.ChoiceField(choices=(('', _('Choose an organization type')),) + Organization2.KINDS, required=False)
     country = forms.CharField(required=False)
@@ -13,6 +14,7 @@ class OrganizationFilterForm(forms.Form):
 
 
 class ProjectFilterForm(forms.Form):
+    pk = forms.IntegerField(required=False)
     name = forms.CharField(required=False)
     activity = forms.ModelChoiceField(queryset=Activity2.objects.all(), required=False, empty_label=_('Choose an activity'))
     organization = forms.CharField(required=False)
@@ -22,6 +24,7 @@ class ProjectFilterForm(forms.Form):
 
 
 class InvestmentFilterForm(forms.Form):
+    pk = forms.IntegerField(required=False)
     kind = forms.ChoiceField(choices=(('', _('Choose an investment type')),) + Investment2.KINDS, required=False)
     organization = forms.CharField(required=False)
     project = forms.CharField(required=False)
