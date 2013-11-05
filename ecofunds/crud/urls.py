9c39418826d2e5c48b1c90ffcec5accf50aff6e8
Henrique Bastos <henrique@bastos.net>
2013-11-05 20:36:28 -0200

    Remove duplicated endpoint

--- ecofunds/crud/urls.py
+++ ecofunds/crud/urls.py
@@ -5,7 +5,4 @@ urlpatterns = patterns('ecofunds.crud.views',
     url(r'^investment/(?P<investment_id>\d+)', 'investment_detail', name="investment_detail"),
     url(r'^project/(?P<project_id>\d+)', 'project_detail', name="project_detail"),
     url(r'^organization/(?P<organization_id>\d+)', 'organization_detail', name="organization_detail"),
-    url(r'^api/project/(?P<project_id>\d+)', 'project_detail_json', name="project_json"),
-    url(r'^api/investment/(?P<investment_id>\d+)', 'investment_detail_json', name="investment_json"),
-    url(r'^api/organization/(?P<organization_id>\d+)', 'organization_detail_json', name="organization_json"),
 )
